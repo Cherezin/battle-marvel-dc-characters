8574fe6f32b28654f7f2cdc516f1075c15f0a8f7
Chere <alexandre2405@gmail.com>
2024-08-06 19:44:39 -0300

    Adicionando novos personagens.

--- front-end/src/pages/home/list-heros.tsx
+++ front-end/src/pages/home/list-heros.tsx
@@ -29,19 +29,21 @@ export function ListHero({
                 {isFilteredHeros.map(hero => {
                     const isSelected = herosSelect.some(selectedHero => selectedHero.id == hero.id)
                     return(
-                        <li className={`${isSelected ? 'bg-zinc-400' : 'bg-zinc-200 hover:bg-zinc-300 ' } rounded-md px-3 py-2`} key={hero.id}>
-                            <div className="flex flex-col items-center justify-center space-y-2 h-full">
-                                <h2 className="font-semibold">{hero.nome}</h2>
-                                <button onClick={() => addHerosSelect(hero.id)} >
-                                    <img className="rounded-md" src={hero.img} alt={hero.nome} />
-                                </button>
-                                <div>
-                                    <button onClick={() => openInfoModal(hero.id)}>
-                                        <Info className="size-5 text-yellow-950 hover:text-yellow-500" />
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
+                        <li className={`${isSelected ? 'bg-zinc-400' : 'bg-zinc-200 hover:bg-zinc-300' } rounded-md px-3 py-2`} key={hero.id}>
+    <div className="flex flex-col items-center justify-between h-full space-y-2">
+        <div className="flex flex-col items-center space-y-2">
+            <h2 className="font-semibold">{hero.nome}</h2>
+            <button onClick={() => addHerosSelect(hero.id)} >
+                <img className="rounded-md" src={hero.img} alt={hero.nome} />
+            </button>
+        </div>
+        <div>
+            <button onClick={() => openInfoModal(hero.id)}>
+                <Info className="size-5 text-yellow-950 hover:text-yellow-500" />
+            </button>
+        </div>
+    </div>
+</li>
                     )
                 })}
             </ul>
